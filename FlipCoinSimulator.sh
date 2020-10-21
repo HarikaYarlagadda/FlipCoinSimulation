c187ad9242e5606fecf7ff10aa0b0973ce956865
HarikaYarlagadda <hariyhari1999@gmail.com>
2020-10-21 14:04:38 +0530

    added function to do until difference 2 if tie

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,4 +1,6 @@
 #! /bin/bash -x
+again ()
+{
 tailcount=0
 headcount=0
 while (( $headcount != 21 && $tailcount != 21 ))
@@ -15,6 +17,9 @@ fi
 done
 echo "no of times tails won is" $tailcount
 echo "no of times heads won is" $headcount
+}
+difference=$(( $headcount - $tailcount ))
+again
 if [ $headcount -gt $tailcount ]
 then
 	echo " head won by"=$(( $headcount-$tailcount ))
@@ -23,4 +28,8 @@ then
 	echo "tail won by "=$(( $tailcount-$headcount ))
 else
 	echo "tie between heads and tails"
+	while [[ $difference -le 2 ]]
+	do
+		again
+	done
 fi
