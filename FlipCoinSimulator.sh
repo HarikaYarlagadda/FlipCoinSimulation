bf74ec42ad2d56711d9e5c4e2cf0a91c1fc53998
HarikaYarlagadda <hariyhari1999@gmail.com>
2020-10-21 13:11:43 +0530

    number of times head and tail has won

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,8 +1,17 @@
 #! /bin/bash -x
+tailcount=0
+headcount=0
+for (( i=0 ; i<30 ; i++ ))
+do
 coin=$(( RANDOM%2 ))
 if (( $coin == 0 ))
 then
 	echo "tails"
+	(( tailcount++ ))
 else
 	echo "heads"
+	(( headcount++ ))
 fi
+done
+echo "no of times tails won is" $tailcount
+echo "no of times heads won is" $headcount
