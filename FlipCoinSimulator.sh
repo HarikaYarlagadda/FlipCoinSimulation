102ffd4a3931e3a730ac7fbca765cbdeccd4609c
HarikaYarlagadda <hariyhari1999@gmail.com>
2020-11-09 13:12:20 +0530

    flipping a coin to display heads or tails wins

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,35 +1,9 @@
-#! /bin/bash -x
-again ()
-{
-tailcount=0
-headcount=0
-while (( $headcount != 21 && $tailcount != 21 ))
-do
+#! /bin/bash 
 coin=$(( RANDOM%2 ))
 if (( $coin == 0 ))
 then
 	echo "tails"
-	(( tailcount++ ))
 else
 	echo "heads"
-	(( headcount++ ))
 fi
-done
-echo "no of times tails won is" $tailcount
-echo "no of times heads won is" $headcount
-}
-difference=$(( $headcount - $tailcount ))
-again
-if [ $headcount -gt $tailcount ]
-then
-	echo " head won by"=$(( $headcount-$tailcount ))
-elif [$headcount -lt $tailcount ]
-then
-	echo "tail won by "=$(( $tailcount-$headcount ))
-else
-	echo "tie between heads and tails"
-	while [[ $difference -le 2 ]]
-	do
-		again
-	done
-fi
+
